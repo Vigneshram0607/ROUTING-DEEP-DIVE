780b18fd8e4b4e5883b86187e0d5b2c838e6c4a4
vigneshram0706 <vigneshram0706@gmail.com>
2025-06-18 10:18:45 +0000

    Splittinf route definitions across multiple files

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,9 @@
 import { Routes } from "@angular/router";
-import { TasksComponent } from "./tasks/tasks.component";
 import { NoTaskComponent } from "./tasks/no-task/no-task.component";
 import { UserTasksComponent } from "./users/user-tasks/user-tasks.component";
-import { NewTaskComponent } from "./tasks/new-task/new-task.component";
 import { NotFoundComponent } from "./not-found/not-found.component";
+
+import { routes as userRoutes } from "./users/users.routes";
 
 export const routes:Routes = [
     {
@@ -13,16 +13,7 @@ export const routes:Routes = [
     {
     path:'users/:userId', // <domain> /users/:userId
     component: UserTasksComponent,
-    children:[
-        { 
-            path:'tasks', // <domain>/users/:userId/ tasks
-            component:TasksComponent
-        },
-        { 
-            path:'tasks/new', // <domain>/users/:userId/ tasks/new
-            component:NewTaskComponent
-        },
-    ]
+    children: userRoutes
 },
 {
     path:'**',
