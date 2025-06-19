ea9778d27f73c702aafe6c64f5f5061afe039069
vigneshram0706 <vigneshram0706@gmail.com>
2025-06-19 12:23:52 +0000

    Introducing canMatch routeguard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,17 @@
-import { Routes } from "@angular/router";
+import { CanMatchFn, RedirectCommand, Router, Routes } from "@angular/router";
 import { NoTaskComponent } from "./tasks/no-task/no-task.component";
 import { resolveTitle, resolveUserName as resolveUserName, UserTasksComponent } from "./users/user-tasks/user-tasks.component";
 import { NotFoundComponent } from "./not-found/not-found.component";
 
 import { routes as userRoutes } from "./users/users.routes";
+import { inject } from "@angular/core";
+
+const dummyCanMatch: CanMatchFn = (route, segments) => {
+    const router = inject(Router);
+    const shouldGetAccess = Math.random();
+    if(shouldGetAccess < 0.5) return true
+    return new RedirectCommand(router.parseUrl('/unauthorized'));
+}
 
 export const routes:Routes = [
     {
@@ -14,6 +22,7 @@ export const routes:Routes = [
     path:'users/:userId', // <domain> /users/:userId
     component: UserTasksComponent,
     children: userRoutes,
+    canMatch: [dummyCanMatch],
     data:{
         message:'Hello World!'
     },
